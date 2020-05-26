25e588c7088f489f8ded7ea258e3d1c9ec3b129f
Sophie Evans <emailsophiep@gmail.com>
2020-05-26 14:39:07 -0300

    add files for challenge Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,31 @@ class TasksController < ApplicationController
     @task = Task.new # needed to instantiate the form_for
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
