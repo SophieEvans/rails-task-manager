b61d8311b9e9b84a1415a84519f8c61c1cff85bf
Sophie Evans <emailsophiep@gmail.com>
2020-05-26 14:51:10 -0300

    add files for challenge Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,6 +25,14 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
   def show
     @task = Task.find(params[:id])
   end
